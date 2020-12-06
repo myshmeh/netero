f3e0cfff5ddc8f8aa81f207e21b57729b137c159
yuki-misumi-sbisecsol <yuki.misumi@sbisecsol.com>
2020-12-06 15:04:40 +0900

    initial commit

---
+++ neterosetting/src/main/java/io/myshmeh/github/neterosetting/NeterosettingApplication.java
@@ -0,0 +1,13 @@
+package io.myshmeh.github.neterosetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeterosettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeterosettingApplication.class, args);
+	}
+
+}
---
+++ neterosetting/src/test/java/io/myshmeh/github/neterosetting/NeterosettingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.myshmeh.github.neterosetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeterosettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
