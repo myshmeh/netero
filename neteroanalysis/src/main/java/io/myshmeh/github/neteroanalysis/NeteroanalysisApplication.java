d20b593e29c5d6236f219cab8c653790b30a879a
yuki-misumi-sbisecsol <yuki.misumi@sbisecsol.com>
2020-12-06 15:13:35 +0900

    added neteroanalysis project

---
+++ neteroanalysis/src/main/java/io/myshmeh/github/neteroanalysis/NeteroanalysisApplication.java
@@ -0,0 +1,13 @@
+package io.myshmeh.github.neteroanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeteroanalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeteroanalysisApplication.class, args);
+	}
+
+}
---
+++ neteroanalysis/src/test/java/io/myshmeh/github/neteroanalysis/NeteroanalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package io.myshmeh.github.neteroanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeteroanalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
