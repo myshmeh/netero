7eaaa352970a5a3d780feb28c0f0fb89e6cfefe7
yuki-misumi-sbisecsol <yuki.misumi@sbisecsol.com>
2020-12-06 15:11:10 +0900

    added neterorecording project

---
+++ neterorecording/src/main/java/io/myshmeh/github/neterorecording/NeterorecordingApplication.java
@@ -0,0 +1,13 @@
+package io.myshmeh.github.neterorecording;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeterorecordingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeterorecordingApplication.class, args);
+	}
+
+}
---
+++ neterorecording/src/test/java/io/myshmeh/github/neterorecording/NeterorecordingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.myshmeh.github.neterorecording;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeterorecordingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
